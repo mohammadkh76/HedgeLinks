86f009e2a4518bea924852d2038e6e508759d693
Mohammad <mohammad.kh76@gmail.com>
2019-04-07 21:00:13 +0430

    initial

---
+++ coderush/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using coderush.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace coderush.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return RedirectToAction("UserProfile", "UserRole");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
