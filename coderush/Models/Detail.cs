99e773d0f157c62168e9791e9b5a3053925b7c13
Mohammad <mohammad.kh76@gmail.com>
2019-05-05 00:53:34 +0430

    in home

---
+++ coderush/Models/Detail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HedgeLinks.Models
+{
+    public class Detail
+    {
+        public string CreatedBy { get; set; }
+        public string EditedBy { get; set; }
+        public string CreateDate { get; set; }
+        public string EditDate { get; set; }
+    }
+}
