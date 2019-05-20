28463c7abe680b47c283bcb6c9c014ac65c597c5
Mohammad <mohammad.kh76@gmail.com>
2019-05-20 08:37:30 +0430

    NN

---
+++ coderush/wwwroot/App/User/insert-user-controller.js
@@ -0,0 +1,16 @@
+﻿adminModule.controller('InsertUserController', ['$scope', '$uibModalInstance', 'dataService', '$window', 'toaster', 'Upload', function ($scope, $uibModalInstance, dataService, $window, toaster, Upload) {
+    console.log("user insert modal")
+    $scope.upload = function (file) {
+        Upload.upload({
+            url: 'upload/url',
+            data: { file: file, 'username': $scope.username }
+        }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        });
+    };
+}])
---
+++ coderush/wwwroot/App/User/user-controller.js
@@ -0,0 +1,108 @@
+﻿adminModule.controller("userController", ["$scope", "dataService", "$window", "$uibModal", "toaster", function ($scope, dataService, $window, $uibModal, toaster) {
+    $scope.currentPage = 1;
+    $scope.tableLoading = true;
+    $scope.page = {
+        Current: $scope.currentPage,
+        ItemInPage: 10
+    }
+    $scope.getAll = function ({ successFunc, messages }) {
+        dataService.post('/api/User/GetAll/', $scope.page).then(function (res) {
+            console.log(res.data);
+            if (res.data.Data.length > 0) {
+                $scope.data = res.data.Data;
+                $scope.totalItems = res.data.Count;
+            }
+            successFunc && successFunc();
+            messages && messages();
+
+        })
+
+    }
+    $scope.getAll({
+        successFunc() {
+            $scope.tableLoading = false;
+        }
+    });
+
+
+    $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+    };
+
+
+    $scope.pageChanged = function (current) {
+        $scope.tableLoading = true;
+        $scope.page.Current = $scope.currentPage;
+        //dataService.post('/api/Menupath/GetAll/', $scope.page2).then(function (res) {
+        //    $scope.tableLoading = false;
+        //    console.log(res.data);
+        //    if (res.data.Data.length > 0) {
+        //        $scope.data = res.data.Data;
+        //        $scope.totalItems = res.data.Count;
+        //    }
+        //})
+        $scope.getAll({
+            successFunc() {
+                $scope.tableLoading = false;
+            }
+        });
+    };
+    $scope.maxSize = 5;
+    //$scope.openDescriptionModal = function (id) {
+    //    $scope.selectedId = id;
+    //    $scope.url = '/api/Menupath/DescriptionDetail/' + id;
+    //    var modalInstance = $uibModal.open({
+    //        animation: true,
+    //        templateUrl: '/App/MenuPath/description-menupath.html',
+    //        controller: 'descriptionMenupathModalController',
+    //        scope: $scope,
+    //        size: 'lg',
+    //        windowClass: 'delete-modal',
+    //        appendTo: $('body')
+    //    })
+    //}
+    $scope.openDeleteModal = function (id) {
+        $scope.selectedId = id;
+        $scope.url = '/api/User/Delete/' + id;
+        $scope.href = '/User/index';
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: '/App/DeleteModal/delete-modal.html',
+            controller: 'deleteModalController',
+            scope: $scope,
+            size: 'lg',
+            windowClass: 'delete-modal',
+            appendTo: $('body')
+        })
+    }
+
+    $scope.openInsertModal = function () {
+        $scope.url = '/api/User/Insert/';
+        $scope.href = '/Users/index';
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: '/App/User/insert-user-modal.html',
+            controller: 'InsertUserController',
+            scope: $scope,
+            size: 'lg',
+            windowClass: 'insert-modal-modal',
+            appendTo: $('body')
+
+        })
+    }
+    $scope.openEditModal = function (id) {
+        $scope.selectedId = id;
+        $scope.url = '/api/Users/GetEditData/' + id;
+        $scope.href = '/Users/index';
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: '/App/Menupath/edit-menupath.html',
+            controller: 'EditMenupathModalController',
+            scope: $scope,
+            size: 'lg',
+            windowClass: 'delete-modal',
+            appendTo: $('body')
+        })
+    }
+    
+    }]);
